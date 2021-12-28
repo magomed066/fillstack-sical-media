8658b68208f8bc1f52aaaf9557a1a96666bb50f9
Magomed <mamergov01@bk.ru>
2021-12-28 14:52:08 +0300

    prapared server fot further work

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
+
+dotenv.config()
+connectDB()
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', async (req, res) => {
+	res.send('API is running!')
+})
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+	console.log(`Server is running on ${PORT} port.`.yellow.bold)
+})
